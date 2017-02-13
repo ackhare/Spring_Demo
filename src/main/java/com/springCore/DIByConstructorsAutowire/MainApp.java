531716dc70f15f345f0af127649c0bd9d5b4129c
chetan <bairagisirfire@gmail.com>
2017-02-13 13:03:48 +0530

    replaced log4j with logback

--- src/main/java/com/springCore/DIByConstructorsAutowire/MainApp.java
+++ src/main/java/com/springCore/DIByConstructorsAutowire/MainApp.java
@@ -1,9 +1,10 @@
 package com.springCore.DIByConstructorsAutowire;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+//import org.apache.log4j.Logger;
+//import org.apache.log4j.PropertyConfigurator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-import  com.springCore.Utility.utility;
+//import  com.springCore.Utility.utility;
+
 import java.io.FileInputStream;
 import java.util.Properties;
 
@@ -14,9 +15,9 @@ public class MainApp {
 //   String log4jConfPath = "/path/to/log4j.properties";
 
 
-   static Logger log = Logger.getLogger(MainApp.class.getName());
+   //static Logger log = Logger.getLogger(MainApp.class.getName());
    public static void main(String[] args) {
-      utility.loadLogger();
+//      utility.loadLogger();
       ApplicationContext context =
              new FileSystemXmlApplicationContext("//home/chetan/Downloads/Spring-Core-master/src/main/java/com/springCore/DIByConstructorsAutowire/Beans.xml");
 
--- src/main/java/com/springCore/javaConfig/MainApp.java
+++ src/main/java/com/springCore/javaConfig/MainApp.java
@@ -4,13 +4,14 @@ package com.springCore.javaConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.springCore.knights.Knight.Knight;
 import org.springframework.context.support.
         ClassPathXmlApplicationContext;
 
 public class MainApp {
-
+    private final Logger logger = LoggerFactory.getLogger(MainApp.class);
     public static void main(String[] args) throws Exception {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
         CDPlayer player = context.getBean(CDPlayer.class);
